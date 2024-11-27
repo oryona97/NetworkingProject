28833d7af390e7e17883fc5d373e89acd2e95e8b
“Or <oryona97@gmail.com>
2024-11-27 16:47:21 +0200

    Created controllers,models,views

--- eBookStore/obj/Debug/net8.0/eBookStore.AssemblyInfo.cs
+++ eBookStore/obj/Debug/net8.0/eBookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eBookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+900b59c39147a4a33e07203e9b23c33e21782fea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff184b58ec5bc5beea23521394dae278489984e")]
 [assembly: System.Reflection.AssemblyProductAttribute("eBookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eBookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
