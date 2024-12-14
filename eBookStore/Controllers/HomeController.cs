afa5213a3c458b691640eba06fc49c5b1bd0d04f
Maxwell Knight <maxwell.knight98@gmail.com>
2024-12-15 00:01:07 +0200

    modified home controller to use login/register view models + updated the style of the layout and logn/register forms

--- eBookStore/Controllers/HomeController.cs
+++ eBookStore/Controllers/HomeController.cs
@@ -1,137 +1,156 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using eBookStore.Models;
+using eBookStore.Models.ViewModels;
 using Microsoft.Data.SqlClient;
 
 namespace eBookStore.Controllers;
-//this calss represent user
+
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly IConfiguration _configuration;
-    private string? connectionString;
-    public HomeController(IConfiguration configuration ,ILogger<HomeController> logger )
-    {
-        _configuration = configuration;
-        connectionString = _configuration.GetConnectionString("DefaultConnection");
-        _logger = logger;
-    }
-    
-    //register
-    [HttpGet]
-    public IActionResult register()
-    {
-        return View();
-    }
-    [HttpPost]
-    public IActionResult register(UserModel user)
-    {
-        try
-        {
-            if(ModelState.IsValid)
-            {
-            using(SqlConnection  connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    Console.WriteLine("Connection successful!");
-                    //this query insert the detais of user
-                    string queries = "INSERT INTO [User] (Username, Password, Email, FirstName, LastName, PhoneNumber)  VALUES (@Username, @Password,@Email,@FirstName,@LastName,@PhoneNumber);";
-                    using(SqlCommand command = new SqlCommand(queries , connection))
-                    {
-                        command.Parameters.AddWithValue("@Username",user.username);
-                        command.Parameters.AddWithValue("@Password",user.password);
-                        command.Parameters.AddWithValue("@Email",user.email);
-                        command.Parameters.AddWithValue("@FirstName",user.firstName);
-                        command.Parameters.AddWithValue("@LastName",user.lastName);
-                        command.Parameters.AddWithValue("@PhoneNumber",user.phoneNumber);
+	private readonly ILogger<HomeController> _logger;
+	private readonly IConfiguration _configuration;
+	private string? connectionString;
 
-                        int rowAffect = command.ExecuteNonQuery();
-                    }
-                    connection.Close();
-                }
-                // show the details to the user
-                return View("showRegister" , user);
-            }
-        }catch(SqlException ex)
-        {
-            Console.WriteLine($"Connection failed: {ex.Message}");
-            return View(user);
-        }
-            //if not valid get the details agian 
-            return View(user);
-    }
-    //**********************************************************************//
+	public HomeController(IConfiguration configuration, ILogger<HomeController> logger)
+	{
+		_configuration = configuration;
+		connectionString = _configuration.GetConnectionString("DefaultConnection");
+		_logger = logger;
+	}
 
-    [HttpGet]
-    //login
-    public IActionResult showLogIn()
-    {
-        Console.WriteLine("showlogin");
-        UserModel user =new UserModel();
-        return View("LogIn",user);        
-    }
-    
-    [HttpPost]
-    public IActionResult LogIn()
-    {
-        UserModel user =new UserModel();
-        //get the userName and password from the form
-        string? userName = Request.Form["userName"].ToString();
-        string? password = Request.Form["password"].ToString();
-        Console.WriteLine("login");
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            connection.Open();
-            Console.WriteLine("Connection successful!");
-            //this quary 
-            string query = "SELECT * FROM [User] WHERE Username = @username;";
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
-                
-                command.Parameters.AddWithValue("@username",userName);
-                Console.WriteLine(userName);
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    if(reader.Read())
-                    {
-                        if(userName == reader["Username"].ToString() && password == reader["Password"].ToString())
-                        {
-                            //init the user details
-                            user.createAt = DateTime.Parse(reader["createAd"].ToString());
-                            user.id =int.Parse(reader["id"].ToString());
-                            user.username = reader["Username"].ToString();
-                            user.password = reader["password"].ToString();
-                            user.email = reader["Email"].ToString();
-                            user.firstName = reader["firstName"].ToString();
-                            user.lastName = reader["lastName"].ToString();
-                            user.phoneNumber = reader["phoneNumber"].ToString();
-                            //show to user 
-                            Console.WriteLine("login success");
-                            return View("showLogIn",user);
-                        }
-                    }
-                        
-                }
-            }
-            connection.Close();
-            
-        }
-        Console.WriteLine("login failed");
-        //if not valid get again
-        return View(user);
-    }
-    //**********************************************************************//
+	// Register Actions
+	[HttpGet]
+	public IActionResult register()
+	{
+		return View(new RegisterViewModel());
+	}
 
+	[HttpPost]
+	public IActionResult register(RegisterViewModel model)
+	{
+		try
+		{
+			if (ModelState.IsValid)
+			{
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					string queries = @"INSERT INTO [User] 
+                    (Username, Password, Email, FirstName, LastName, PhoneNumber)  
+                    VALUES (@Username, @Password, @Email, @FirstName, @LastName, @PhoneNumber);";
 
+					using (SqlCommand command = new SqlCommand(queries, connection))
+					{
+						command.Parameters.AddWithValue("@Username", model.Username);
+						command.Parameters.AddWithValue("@Password", model.Password);
+						command.Parameters.AddWithValue("@Email", model.Email);
+						command.Parameters.AddWithValue("@FirstName", model.FirstName);
+						command.Parameters.AddWithValue("@LastName", model.LastName);
+						command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+						int rowAffect = command.ExecuteNonQuery();
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+						if (rowAffect > 0)
+						{
+							// Registration successful, convert to UserModel if needed
+							var userModel = new UserModel
+							{
+								username = model.Username,
+								password = model.Password,
+								email = model.Email,
+								firstName = model.FirstName,
+								lastName = model.LastName,
+								phoneNumber = model.PhoneNumber
+							};
+
+							return View("showRegister", userModel);
+						}
+					}
+				}
+			}
+		}
+		catch (SqlException ex)
+		{
+			_logger.LogError(ex, "Registration failed");
+			ModelState.AddModelError("", "Registration failed. Please try again.");
+		}
+
+		return View(model);
+	}
+
+	// Login Actions
+	[HttpGet]
+	public IActionResult showLogin()
+	{
+		return View("LogIn", new LoginViewModel());
+	}
+
+	[HttpPost]
+	public IActionResult LogIn(LoginViewModel model)
+	{
+		if (ModelState.IsValid)
+		{
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					string query = "SELECT * FROM [User] WHERE Username = @username;";
+
+					using (SqlCommand command = new SqlCommand(query, connection))
+					{
+						command.Parameters.AddWithValue("@username", model.Username);
+
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							if (reader.Read())
+							{
+								string storedPassword = reader["Password"]?.ToString() ?? string.Empty;
+
+								if (model.Password == storedPassword) // Note: Consider using proper password hashing
+								{
+									// Login successful
+									var userModel = new UserModel
+									{
+										username = reader["Username"]?.ToString(),
+										email = reader["Email"]?.ToString(),
+										firstName = reader["FirstName"]?.ToString(),
+										lastName = reader["LastName"]?.ToString(),
+										phoneNumber = reader["PhoneNumber"]?.ToString(),
+										id = Convert.ToInt32(reader["id"])
+									};
+
+									// If you need to show user details after login
+									return View("showLogIn", userModel);
+								}
+							}
+						}
+					}
+
+					// Invalid login
+					ModelState.AddModelError("", "Invalid username or password");
+				}
+			}
+			catch (SqlException ex)
+			{
+				_logger.LogError(ex, "Database error during login");
+				ModelState.AddModelError("", "An error occurred during login. Please try again.");
+			}
+		}
+
+		// If we got this far, something failed
+		return View("LogIn", model);
+	}
+
+	public IActionResult Privacy()
+	{
+		return View();
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 }
