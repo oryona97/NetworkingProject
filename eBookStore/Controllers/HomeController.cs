08e8e525eabd2069c5e2ae32baeb8946c394a208
“Or <oryona97@gmail.com>
2024-12-31 12:20:07 +0200

    Fixed shoppingCartController + home controller

--- eBookStore/Controllers/HomeController.cs
+++ eBookStore/Controllers/HomeController.cs
@@ -166,30 +166,6 @@ public class HomeController : Controller
 	{
 		return View("SearchBooks");
 	}
-	public IActionResult ShowShoppingCart()
-	{
-		try
-		{
-			int? nullableUserId = HttpContext.Session.GetInt32("userId");
-			Console.WriteLine("the user id is: ");
-			Console.WriteLine(nullableUserId.Value);
-			if (nullableUserId.HasValue)
-			{
-				Console.WriteLine("Ok ");
-				ShoppingCartViewModel cart = shoppingCartRepo.GetShoppingCart(nullableUserId.Value);
-				return View("ShoppingCart/ShowShoppingCart", cart);
-			}
-			else
-			{
-				return RedirectToAction("showLogIn");
-			}
-		}
-		catch (Exception ex)
-		{
-			_logger.LogError(ex, "Failed to load shopping cart");
-			return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
 
 	public IActionResult Profile()
 	{
