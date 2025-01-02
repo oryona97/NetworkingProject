759f2a5b2217beeef44a039d1b3453edf145e4ab
“Or <oryona97@gmail.com>
2025-01-02 15:37:59 +0200

    Gallery refactor

--- eBookStore/Controllers/GalleryController.cs
+++ eBookStore/Controllers/GalleryController.cs
@@ -1,21 +1,39 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using eBookStore.Models;
+using eBookStore.Models.ViewModels;
+using eBookStore.Repository;
+
 
 namespace eBookStore.Controllers;
 
 public class GalleryController : Controller
 {
-    private readonly ILogger<GalleryController> _logger;
 
-    public GalleryController(ILogger<GalleryController> logger)
-    {
-        _logger = logger;
-    }
+	private readonly ILogger<HomeController> _logger;
+	private readonly IConfiguration _configuration;
+	private string? connectionString;
+	private BookRepository _bookRepo;
+	private UserRepository _userRepo;
+
+	public GalleryController(IConfiguration configuration, ILogger<HomeController> logger)
+	{
+		_configuration = configuration;
+		connectionString = _configuration.GetConnectionString("DefaultConnection");
+		_logger = logger;
+		_bookRepo = new BookRepository(connectionString);
+	}
+
+
 
     public IActionResult Index()
     {
-        return View();
+        GalleryPageViewModel data = new GalleryPageViewModel();
+
+        data.allBooks = _bookRepo.getAllBooks();
+        data.listOfCategorys = _bookRepo.getAllGenres();
+
+        return View(data);
     }
 
     public IActionResult Privacy()
---
+++ eBookStore/Models/ViewModels/GalleryPageViewModel.cs
@@ -0,0 +1,10 @@
+using eBookStore.Models;
+
+namespace eBookStore.Models.ViewModels;
+
+public class GalleryPageViewModel
+{
+    public List<BookViewModel> allBooks {get; set; }
+
+    public List<string> listOfCategorys {get; set;}
+}
