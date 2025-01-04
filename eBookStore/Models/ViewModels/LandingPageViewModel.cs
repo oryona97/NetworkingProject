a4f286ed608e9c9f43e02276733521cdf8cf7d4c
“Or <oryona97@gmail.com>
2025-01-04 23:36:31 +0200

    Added website review system

--- eBookStore/Models/ViewModels/LandingPageViewModel.cs
+++ eBookStore/Models/ViewModels/LandingPageViewModel.cs
@@ -8,6 +8,7 @@ namespace eBookStore.Models.ViewModels
         public List<BookViewModel> SpecialSales { get; set; } // List of books on special sales
         public List<BookViewModel> allBooks {get; set; } // List of all the books in the store - needed for carousel
         public List<string> listOfCategorys {get; set;}
+        public List<GeneralFeedbackModel> allFeedbacks {get; set;}
 
     }
 }
