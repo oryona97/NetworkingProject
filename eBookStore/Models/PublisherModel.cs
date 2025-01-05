d35a7cbc92a571f4d0c65cfa3e8e3c064615fba4
Maxwell Knight <maxwell.knight98@gmail.com>
2025-01-05 13:01:01 +0200

    refactored BookViewModel to contain publishers list instead of a single model

--- eBookStore/Models/PublisherModel.cs
+++ eBookStore/Models/PublisherModel.cs
@@ -6,12 +6,14 @@ namespace eBookStore.Models;
 
 public class PublisherModel
 {
-	[Required(ErrorMessage = "id is required")]
-	public int id { get; set; }
-	
-	[Required(ErrorMessage = "name is required")]
-	public string name { get; set; }
-	public DateTime createdAt { get; set; } = DateTime.Now;
+    [Required(ErrorMessage = "id is required")]
+    public int id { get; set; }
+
+    [Required(ErrorMessage = "name is required")]
+    public string? name { get; set; }
+    [Required(ErrorMessage = "bookId is required")]
+    public int bookId { get; set; }
+    public DateTime createdAt { get; set; } = DateTime.Now;
 }
 
 
